be7bd0e3cdad6793902882d0dc50ef7bafb6e44b
william simonin <simoni_w@eduD89E.kent.ac.uk>
2015-01-24 19:28:55 +0000

    Fix bug emission

---
+++ protocolRadio/Emission/Prog_ino/.build/uno/src/sketch.cpp
@@ -0,0 +1,68 @@
+#include <Arduino.h>
+
+void    sendBit(int pin, unsigned int time);
+void    send(int pin, char *cmd);
+void setup();
+void loop();
+#line 1 "src/sketch.ino"
+
+#define TIMEVERROU      2750    // Time use for lock
+#define TIMEHIGH        275     // Time use for 1
+#define TIMELOW         1225    // Time use for 0
+#define TIMEUP          275     // time use for up signal
+
+// Send Bit
+void    sendBit(int pin, unsigned int time)
+{
+    digitalWrite(pin, HIGH);
+    delayMicroseconds(TIMEUP);
+    digitalWrite(pin, LOW);
+    delayMicroseconds(time);
+}
+
+// Send Bit in Manchester coding
+void    send(int pin, char *cmd)
+{
+    int i;
+
+    // verrou
+    sendBit(pin, TIMEVERROU);
+
+    for (i = 0; cmd[i]; i++)
+    {
+        // Send 1 in MANCHESTER -> "1" = "10"
+        if (cmd[i] == '1')
+        {
+            sendBit(pin, TIMEHIGH);
+            sendBit(pin, TIMELOW);
+        }
+        // Send 0 in MANCHESTER -> "0" = "01"
+        else
+        {
+            sendBit(pin, TIMELOW);
+            sendBit(pin, TIMEHIGH);
+        }
+    }
+}
+
+// Set the pin used to send the output signal
+void setup()
+{
+    pinMode(2, OUTPUT);
+    pinMode(13, OUTPUT);
+    digitalWrite(13,LOW);
+}
+
+// loop sends signal
+void loop()
+{
+    // We send id module and value (4 Bits)
+    // id =     0000000000101010 = 42
+    // module = 00001001 = 9
+    // value =  00000101 = 5
+send(2, "00000000001010100000100100000101");
+digitalWrite(2, HIGH);
+delayMicroseconds(TIMEUP);
+digitalWrite(2, LOW);
+delay(10);
+}
