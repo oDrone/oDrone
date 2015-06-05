43b334cff683f4d211b57fbcae366f1f27e6d23f
Olivier Gayot <duskcoder@gmail.com>
2015-06-05 22:42:36 +0100

    multiwii: added a class to log messages
    Signed-off-by: Olivier Gayot <duskcoder@gmail.com>

---
+++ crius/MultiWii/logs.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <Arduino.h>
+#include "Serial.h"
+
+int serial_write(uint8_t val)
+{
+    SerialWrite(0, val);
+}
+
+int serial_put_string(const char *str)
+{
+    while (*str) {
+        serial_write(*str++);
+    }
+
+    return 0;
+}
+
+int serial_puts(const char *str)
+{
+    serial_put_string(str);
+
+    serial_write('\n');
+
+    return 0;
+}
+
+int serial_printf(const char *fmt, ...)
+{
+    va_list ap;
+
+    static char buf[256];
+
+    va_start(ap, fmt);
+
+    vsnprintf(buf, sizeof(buf), fmt, ap);
+
+    serial_put_string(buf);
+
+    va_end(ap);
+
+    return 0;
+}
+
+#if 0
+int serial_print_dec(uint8_t opcode, int32_t integer)
+{
+    Serial.write(&opcode, 1);
+    Serial.println(integer, DEC);
+    return 0;
+}
+
+int serial_print_hex(uint8_t opcode, int32_t integer)
+{
+    Serial.write(&opcode, 1);
+    Serial.println(integer, HEX);
+    return 0;
+}
+#endif
---
+++ crius/MultiWii/logs.h
@@ -0,0 +1,34 @@
+#ifndef LOGS_H
+#define LOGS_H
+
+#include <stdarg.h>
+
+#if 0
+enum {
+    OPCODE_LOGS = 0x89,
+    OPCODE_OK = 0x70,
+    OPCODE_ERR = 0x70,
+};
+
+#define serial_log serial_log_puts
+
+#define serial_log_puts(_str) serial_puts(OPCODE_LOGS, _str);
+#define serial_log_print_dec(_int) serial_print_dec(OPCODE_LOGS, _int)
+#define serial_log_print_hex(_int) serial_print_dec(OPCODE_LOGS, _int)
+
+#define serial_ok(_str) serial_puts(OPCODE_OK, _str);
+#define serial_err(_str) serial_puts(OPCODE_ERR, _str);
+
+#endif
+
+int serial_puts(const char *str);
+int serial_put_string(const char *str);
+int serial_write(uint8_t value);
+int serial_printf(const char *str, ...);
+
+#if 0
+int serial_print_dec(uint8_t opcode, int32_t integer);
+int serial_print_hex(uint8_t opcode, int32_t integer);
+#endif
+
+#endif
