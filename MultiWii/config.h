d16bd58642fd642a64fbfe1f20d3bb605e783446
Olivier Gayot <duskcoder@gmail.com>
2015-06-05 14:26:15 +0100

    crius: added the configuration of the board
    Signed-off-by: Olivier Gayot <duskcoder@gmail.com>

--- MultiWii/config.h
+++ MultiWii/config.h
@@ -36,7 +36,7 @@
     //#define BI
     //#define TRI
     //#define QUADP
-    //#define QUADX
+#define QUADX
     //#define Y4
     //#define Y6
     //#define HEX6
@@ -125,7 +125,7 @@
       //#define MONGOOSE1_0     // mongoose 1.0    http://store.ckdevices.com/
       //#define CRIUS_LITE      // Crius MultiWii Lite
       //#define CRIUS_SE        // Crius MultiWii SE
-      //#define CRIUS_SE_v2_0   // Crius MultiWii SE 2.0 with MPU6050, HMC5883 and BMP085
+      #define CRIUS_SE_v2_0   // Crius MultiWii SE 2.0 with MPU6050, HMC5883 and BMP085
       //#define OPENLRSv2MULTI  // OpenLRS v2 Multi Rc Receiver board including ITG3205 and ADXL345
       //#define BOARD_PROTO_1   // with MPU6050 + HMC5883L + MS baro
       //#define BOARD_PROTO_2   // with MPU6050 + slave  MAG3110 + MS baro
